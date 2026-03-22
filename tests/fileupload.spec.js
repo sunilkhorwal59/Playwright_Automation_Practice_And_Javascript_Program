96f29eab1fc5ed4c8d0e4c7a02dfb6cf1c7473ed
Sunil Khorwal <sunilkhorwal59@github.com>
2026-03-22 22:15:10 +0530

    Allure reporter added

--- tests/fileupload.spec.js
+++ tests/fileupload.spec.js
@@ -1,16 +1,16 @@
-const {test,expect} = require('@playwright/test')
+const path = require('path');
+const { test, expect } = require('@playwright/test');
 
-test('Verify File Upload', async function({page}){
+test('Verify File Upload', async ({ page }) => {
+    const uploadFilePath = path.resolve(__dirname, '../Uploads/Workflow.jpg');
 
     await page.goto('https://the-internet.herokuapp.com/upload');
 
-    await page.locator('#file-upload').setInputFiles('./Uploads/Workflow.jpg');
+    await page.locator('#file-upload').setInputFiles(uploadFilePath);
 
     await page.locator('#file-submit').click();
 
     await expect(page.locator('h3')).toHaveText('File Uploaded!');
 
     await expect(page.locator('h3')).toBeVisible();
-
-    await page.waitForTimeout(5000);
-})
+});
